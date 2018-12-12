c651584c35c9ba485bd2ddc48e48b22d4452a2a3
Peter Kerpedjiev <pkerpedjiev@gmail.com>
2018-12-11 19:28:49 -0800

    Expose verion in higlass.widget

--- higlass/widgets.py
+++ higlass/widgets.py
@@ -1,19 +1,22 @@
-from ._version import version_info, __version__, EXTENSION_VERSION
 import ipywidgets as widgets
+
 from traitlets import Unicode
 from traitlets import default
 from traitlets import List
 from traitlets import Dict
 from traitlets import Int
 
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
 
 class HiGlassDisplay(widgets.DOMWidget):
     _view_name = Unicode('HiGlassDisplayView').tag(sync=True)
     _model_name = Unicode('HiGlassDisplayModel').tag(sync=True)
     _view_module = Unicode('jupyter-higlass').tag(sync=True)
     _model_module = Unicode('jupyter-higlass').tag(sync=True)
-    _view_module_version = Unicode(EXTENSION_VERSION).tag(sync=True)
-    _model_module_version = Unicode(EXTENSION_VERSION).tag(sync=True)
+    _view_module_version = Unicode(__version__).tag(sync=True)
+    _model_module_version = Unicode(__version__).tag(sync=True)
 
     _model_data = List([]).tag(sync=True)
     viewconf = Dict({}).tag(sync=True)

28d475fe4e949b60d7d64ed426a018dff9142ea6
Peter Kerpedjiev <pkerpedjiev@gmail.com>
2019-12-20 21:38:11 -0500

    Ran black and updated the CHANGELOG

--- test/viewer_test.py
+++ test/viewer_test.py
@@ -3,21 +3,18 @@ from unittest.mock import patch
 from higlass.client import View, Track
 from higlass.viewer import display
 
+
 def test_create_display():
     """Test to make sure we can create a display."""
-    with patch('higlass.server.Server') as _:
-        (_, _, viewconf) = display(
-            [View([Track('top-axis')])]
-        )
+    with patch("higlass.server.Server") as _:
+        (_, _, viewconf) = display([View([Track("top-axis")])])
 
         vc_dict = viewconf.to_dict()
 
-        assert len(vc_dict['views']) == 1
+        assert len(vc_dict["views"]) == 1
 
-        (_, _, viewconf) = display(
-            [[Track('top-axis')]]
-        )
+        (_, _, viewconf) = display([[Track("top-axis")]])
 
         vc_dict = viewconf.to_dict()
 
-        assert len(vc_dict['views']) == 1
+        assert len(vc_dict["views"]) == 1

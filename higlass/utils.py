e1932c9a7f306b539a5292b351652e5da0ee652f
Peter Kerpedjiev <pkerpedjiev@gmail.com>
2020-01-02 21:05:24 -0800

    Added fill_filetype_and_datatype and associated functions

--- higlass/utils.py
+++ higlass/utils.py
@@ -1,4 +1,6 @@
 import matplotlib.pyplot as plt
+import os.path as op
+import sys
 
 
 def hg_cmap(cmap_name, resolution=256, reverse=False):
@@ -23,3 +25,104 @@ def hg_cmap(cmap_name, resolution=256, reverse=False):
         return hg_cmap[::-1]
 
     return hg_cmap
+
+
+def recommend_filetype(filename):
+    ext = op.splitext(filename)
+    if op.splitext(filename)[1] == ".bed":
+        return "bedfile"
+    if op.splitext(filename)[1] == ".bedpe":
+        return "bedpe"
+
+
+def recommend_datatype(filetype):
+    if filetype == "bedfile":
+        return "bedlike"
+
+
+def infer_filetype(filename):
+    _, ext = op.splitext(filename)
+
+    if ext.lower() == ".bw" or ext.lower() == ".bigwig":
+        return "bigwig"
+    elif ext.lower() == ".mcool" or ext.lower() == ".cool":
+        return "cooler"
+    elif ext.lower() == ".htime":
+        return "time-interval-json"
+    elif ext.lower() == ".hitile":
+        return "hitile"
+    elif ext.lower() == ".beddb":
+        return "beddb"
+
+    return None
+
+
+def infer_datatype(filetype):
+    if filetype == "cooler":
+        return "matrix"
+    if filetype == "bigwig":
+        return "vector"
+    if filetype == "time-interval-json":
+        return "time-interval"
+    if filetype == "hitile":
+        return "vector"
+    if filetype == "beddb":
+        return "bedlike"
+
+
+def fill_filetype_and_datatype(filename, filetype=None, datatype=None):
+    """
+    If no filetype or datatype are provided, add them
+    based on the given filename.
+    Parameters:
+    ----------
+    filename: str
+        The name of the file
+    filetype: str
+        The type of the file (can be None)
+    datatype: str
+        The datatype for the data in the file (can be None)
+    Returns:
+    --------
+    (filetype, datatype): (str, str)
+        Filled in filetype and datatype based on the given filename
+    """
+    if filetype is None:
+        # no filetype provided, try a few common filetypes
+        filetype = infer_filetype(filename)
+        print("Inferred filetype:", filetype)
+
+        if filetype is None:
+            recommended_filetype = recommend_filetype(filename)
+
+            print(
+                "Unknown filetype, please specify using the --filetype option",
+                file=sys.stderr,
+            )
+            if recommended_filetype is not None:
+                print(
+                    "Based on the filename, you may want to try the filetype: {}".format(
+                        recommended_filetype
+                    )
+                )
+
+            return (None, None)
+
+    if datatype is None:
+        datatype = infer_datatype(filetype)
+        print("Inferred datatype:", datatype)
+
+        if datatype is None:
+            recommended_datatype = recommend_datatype(filetype)
+            print(
+                "Unknown datatype, please specify using the --datatype option",
+                file=sys.stderr,
+            )
+            if recommended_datatype is not None:
+                print(
+                    "Based on the filetype, you may want to try the datatype: {}".format(
+                        recommended_datatype
+                    )
+                )
+
+    return (filetype, datatype)
---
+++ test/utils_test.py
@@ -0,0 +1,8 @@
+from higlass.utils import fill_filetype_and_datatype
+
+
+def test_fill_filetype_and_datatype():
+    filetype, datatype = fill_filetype_and_datatype("myfile.mcool")
+
+    assert filetype == "cooler"
+    assert datatype == "matrix"

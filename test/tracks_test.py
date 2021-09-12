a4d558b76b62bbbcbdd790a95ec3907e909d661c
Peter Kerpedjiev <pkerpedjiev@gmail.com>
2021-09-11 20:39:37 -0700

    Added tests for beditems track

---
+++ test/tracks_test.py
@@ -0,0 +1,35 @@
+from higlass.tracks import beditems
+from tempfile import TemporaryDirectory
+from os.path import join
+
+def test_beditems():
+	with TemporaryDirectory() as tmpdir:
+		chromfile = join(tmpdir, 'chrom.sizes')
+		with open(chromfile, 'w') as f:
+			f.write("chr1\t1000\n")
+			f.write("chr2\t2000\n")
+			f.flush()
+
+		lines = [
+			("chr1", 10, 100, "a1", ".", "+"),
+			("chr2", 10, 100, "a2", ".", "+"),
+		]
+
+		track = beditems(lines, chromfile)
+
+		tsinfo = track.conf['data']['tilesetInfo']
+
+		assert tsinfo['x']['max_width'] == 3000
+		assert tsinfo['x']['max_pos'][0] == 3000
+
+
+		tiles = track.conf['data']['tiles']
+
+		assert 'x.0.0' in tiles
+		assert len(tiles['x.0.0']) == 2
+		assert tiles['x.0.0'][0]['xStart'] == 10
+
+		# second item's start should be offset by chr1's position
+		assert tiles['x.0.0'][1]['xStart'] == 1010
+		assert tiles['x.0.0'][1]['chrOffset'] == 1000
+

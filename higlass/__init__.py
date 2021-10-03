f311c04cda1f3ee69447e7cf65318e0dc05ae184
Peter Kerpedjiev <pkerpedjiev@gmail.com>
2021-10-03 15:17:17 -0700

    Removed accidentally added piece of code

--- higlass/__init__.py
+++ higlass/__init__.py
@@ -1,27 +1,11 @@
 from ._version import __version__
-from .viewer import display
-from .tilesets import Tileset
+from .client import CombinedTrack, Track, View, ViewConf
 from .server import Server
-from .client import Track, CombinedTrack, View, ViewConf
+from .tilesets import Tileset
+from .viewer import display
 
 
 def _jupyter_nbextension_paths():
-    ViewConf(
-        [
-            View(
-                [
-                    Track(track_type="top-axis"),
-                    Track(
-                        track_type="pileup",
-                        position="top",
-                        data={"type": "bam", "url": "my_bam"},
-                        options={"axisPositionHorizontal": "right"},
-                    ),
-                    Track(track_type="vcf", position="top", data={"type": "vcf", "url": "my_vcf"}),
-                ]
-            )
-        ]
-    ).to_dict()
     return [
         {
             "section": "notebook",

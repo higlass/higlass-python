42a0e25fe340452a996236d12153fba80479cfa3
Trevor Manz <trevor.j.manz@gmail.com>
2022-02-03 15:10:18 -0500

    chore: write _version.py

--- hg/__init__.py
+++ hg/__init__.py
@@ -1,4 +1,9 @@
+try:
+    from ._version import version as __version__
+except ImportError:
+    __version__ = "unknown"
+
 from higlass_schema import *
 
-from .api import *
+from .api import * # overrides classes with same name from higlass_schema
 from .server import HgServer, server

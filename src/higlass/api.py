02e9d4e9c737cc558ee7b973afb42e99ea8d76ea
Trevor Manz <trevor.j.manz@gmail.com>
2023-02-14 10:39:13 -0500

    docstrings

--- src/higlass/api.py
+++ src/higlass/api.py
@@ -1,12 +1,11 @@
+from __future__ import annotations
+
 import functools
 from collections import defaultdict
 from typing import (
     TYPE_CHECKING,
     ClassVar,
     Generic,
-    List,
-    Optional,
-    Tuple,
     TypeVar,
     Union,
     overload,
@@ -54,6 +53,7 @@ class _PropertiesMixin:
     def properties(
         self: utils.ModelT, inplace: bool = False, **fields  # type: ignore
     ) -> utils.ModelT:  # type: ignore
+        """Configures top-level properties."""
         model = self if inplace else utils.copy_unique(self)
         for k, v in fields.items():
             setattr(model, k, v)
@@ -66,6 +66,7 @@ class _OptionsMixin:
         inplace: bool = False,
         **options,
     ) -> "TrackT":  # type: ignore
+        """Configures options for a Track."""
 
         track = self if inplace else utils.copy_unique(self)
         if track.options is None:
@@ -80,6 +81,7 @@ class _TilesetMixin:
         tileset: "TilesetResource",
         inplace: bool = False,
     ) -> "TrackT":  # type: ignore
+        """Binds a tileset to a Track."""
 
         track = self if inplace else utils.copy_unique(self)
         track.server = tileset.server
@@ -127,12 +129,15 @@ TrackT = TypeVar("TrackT", bound=Track)
 
 
 class View(hgs.View[TrackT], _PropertiesMixin, Generic[TrackT]):
+    """A HiGlass View"""
+
     def domain(
         self,
-        x: Optional[hgs.Domain] = None,
-        y: Optional[hgs.Domain] = None,
+        x: hgs.Domain | None = None,
+        y: hgs.Domain | None = None,
         inplace: bool = False,
     ):
+        """Configures the X & Y domain for a HiGlass View"""
         view = self if inplace else utils.copy_unique(self)
         if x is not None:
             view.initialXDomain = x
@@ -140,25 +145,34 @@ class View(hgs.View[TrackT], _PropertiesMixin, Generic[TrackT]):
             view.initialYDomain = y
         return view
 
-    def __or__(self, other: Union["View[TrackT]", "Viewconf[TrackT]"]):
+    def __or__(self, other: "View[TrackT]" | "Viewconf[TrackT]"):
+        """Horizontally concatenate two Views."""
         return hconcat(self, other)
 
-    def __truediv__(self, other: Union["View[TrackT]", "Viewconf[TrackT]"]):
+    def __truediv__(self, other: "View[TrackT]" | "Viewconf[TrackT]"):
+        """Verticall stack two Views."""
         return vconcat(self, other)
 
     def clone(self):
+        """Clone the the View instance.
+
+        Inserts a unique uuid so the cloned View is unique in the front end.
+        """
         return utils.copy_unique(self)
 
     def viewconf(self, **kwargs):
+        """Consumes the current View into a top-level HiGlass view config."""
         return Viewconf[TrackT](views=[self], **kwargs)
 
     def display(self):
+        """Displays the view with HiGlass front end."""
         self.viewconf().display()
 
     def _repr_mimebundle_(self, include=None, exclude=None):
         return self.viewconf()._repr_mimebundle_(include, exclude)
 
     def widget(self, **kwargs):
+        """Displays the view as a Jupyter Widget."""
         return self.viewconf().widget(**kwargs)
 
     def project(
@@ -168,6 +182,32 @@ class View(hgs.View[TrackT], _PropertiesMixin, Generic[TrackT]):
         inplace: bool = False,
         **kwargs,
     ):
+        """Project a different viewport onto this view.
+
+        Creates a viewport-projection track whos bounds are synchonrized
+        with an existing, separate view.
+
+        Parameters
+        ----------
+
+        view : View
+            The view with the desired viewport to project.
+
+        on : Literal["center", "top", "bottom", "left", "right"], optional
+            Where to project the viewport on this view (default: "center").
+
+        inplace : bool
+            Whether to modify this view inplace, or alternatively return
+            a new view (default: `False`).
+
+        **kwargs : dict
+            Additional parameters for the created viewport-projection track.
+
+        Returns
+        -------
+
+        view : A view with a new viewport-projection track.
+        """
         new_view = self if inplace else utils.copy_unique(self)
 
         # projection track type from position
@@ -192,8 +232,8 @@ class View(hgs.View[TrackT], _PropertiesMixin, Generic[TrackT]):
 ViewT = TypeVar("ViewT", bound=View)
 
 
-def gather_plugin_urls(views: List[ViewT]) -> List[str]:
-    """Inspect tracks and extract plugin urls to inject into HTML"""
+def gather_plugin_urls(views: list[ViewT]) -> list[str]:
+    """Inspect tracks and extract plugin urls to inject into HTML."""
     plugin_urls = {}
     for view in views:
         for _, track in view.tracks:
@@ -203,6 +243,8 @@ def gather_plugin_urls(views: List[ViewT]) -> List[str]:
 
 
 class Viewconf(hgs.Viewconf[View[TrackT]], _PropertiesMixin, Generic[TrackT]):
+    """Represents a complete HiGlass visualization."""
+
     def _repr_mimebundle_(self, include=None, exclude=None):
         renderer = display.renderers.get()
         plugin_urls = [] if self.views is None else gather_plugin_urls(self.views)
@@ -215,12 +257,14 @@ class Viewconf(hgs.Viewconf[View[TrackT]], _PropertiesMixin, Generic[TrackT]):
         display(self)
 
     def widget(self, **kwargs):
+        """Returns a Jupyter Widget for this view config."""
         from higlass_widget import HiGlassWidget
 
         return HiGlassWidget(self.dict())  # type: ignore
 
     @classmethod
     def from_url(cls, url: str):
+        """Loads a view config via URL."""
         import urllib.request as urllib
 
         request = urllib.Request(url)
@@ -231,21 +275,27 @@ class Viewconf(hgs.Viewconf[View[TrackT]], _PropertiesMixin, Generic[TrackT]):
 
     def locks(
         self,
-        *locks: Union[hgs.Lock, hgs.ValueScaleLock],
-        zoom: Optional[Union[List[hgs.Lock], hgs.Lock]] = None,
-        location: Optional[Union[List[hgs.Lock], hgs.Lock]] = None,
-        value_scale: Optional[
-            Union[List[hgs.ValueScaleLock], hgs.ValueScaleLock]
-        ] = None,
+        *locks: hgs.Lock | hgs.ValueScaleLock,
+        zoom: list[hgs.Lock] | hgs.Lock | None = None,
+        location: list[hgs.Lock] | hgs.Lock | None = None,
+        value_scale: list[hgs.ValueScaleLock] | hgs.ValueScaleLock | None = None,
         inplace: bool = False,
     ):
+        """Specify view locks.
+
+        Parameters:
+        -----------
+
+        locks,
+
+        """
         conf = self if inplace else utils.copy_unique(self)
 
         zoom = utils.ensure_list(zoom)
         location = utils.ensure_list(location)
         value_scale = utils.ensure_list(value_scale)
 
-        shared_locks: List[hgs.Lock] = []
+        shared_locks: list[hgs.Lock] = []
         for lock in locks:
             if isinstance(lock, hgs.Lock):
                 shared_locks.append(lock)
@@ -284,21 +334,19 @@ class Viewconf(hgs.Viewconf[View[TrackT]], _PropertiesMixin, Generic[TrackT]):
 
         return conf
 
-    def __or__(
-        self, other: Union[View[TrackT], "Viewconf[TrackT]"]
-    ) -> "Viewconf[TrackT]":
+    def __or__(self, other: View[TrackT] | Viewconf[TrackT]) -> Viewconf[TrackT]:
+        """Horizontally concatenate with another view config or View."""
         return hconcat(self, other)
 
-    def __truediv__(
-        self, other: Union[View[TrackT], "Viewconf[TrackT]"]
-    ) -> "Viewconf[TrackT]":
+    def __truediv__(self, other: View[TrackT] | Viewconf[TrackT]) -> Viewconf[TrackT]:
+        """Verticallt concatenate with another view config or View."""
         return vconcat(self, other)
 
 
 def concat(
     method: Literal["horizontal", "vertical"],
-    a: Union[View[TrackT], Viewconf[TrackT]],
-    b: Union[View[TrackT], Viewconf[TrackT]],
+    a: View[TrackT] | Viewconf[TrackT],
+    b: View[TrackT] | Viewconf[TrackT],
 ):
     a = a.viewconf() if isinstance(a, View) else a
     assert a.views is not None
@@ -357,7 +405,7 @@ class _TrackCreator(BaseModel):
 
 def track(
     type_: utils.TrackType,
-    uid: Optional[str] = None,
+    uid: str | None = None,
     **kwargs,
 ) -> Track:
     if uid is None:
@@ -367,18 +415,14 @@ def track(
 
 
 def view(
-    *_tracks: Union[
-        TrackT,
-        Tuple[TrackT, utils.TrackPosition],
-        hgs.Tracks[TrackT],
-    ],
+    *_tracks: TrackT | tuple[TrackT, utils.TrackPosition] | hgs.Tracks[TrackT],
     x: int = 0,
     y: int = 0,
     width: int = 12,
     height: int = 6,
-    tracks: Optional[hgs.Tracks[TrackT]] = None,
-    layout: Optional[hgs.Layout] = None,
-    uid: Optional[str] = None,
+    tracks: hgs.Tracks[TrackT] | None = None,
+    layout: hgs.Layout | None = None,
+    uid: str | None = None,
     **kwargs,
 ) -> View[TrackT]:
 
@@ -418,7 +462,7 @@ def view(
     )
 
 
-def combine(t1: Track, t2: Track, uid: Optional[str] = None, **kwargs) -> CombinedTrack:
+def combine(t1: Track, t2: Track, uid: str | None = None, **kwargs) -> CombinedTrack:
     if uid is None:
         uid = utils.uid()
 
@@ -473,11 +517,12 @@ def lock(*views: View, **kwargs) -> hgs.Lock:
 
 
 @overload
-def lock(*pairs: Tuple[View, Track], **kwargs) -> hgs.ValueScaleLock:
+def lock(*pairs: tuple[View, Track], **kwargs) -> hgs.ValueScaleLock:
     ...
 
 
-def lock(*data, uid: Optional[str] = None, **kwargs):
+def lock(*data, uid: str | None = None, **kwargs):
+    """Specify locks to synchronize multiple views."""
     assert len(data) >= 1
     if uid is None:
         uid = utils.uid()
--- test/test_api.py
+++ test/test_api.py
@@ -5,3 +5,15 @@ def test_basic_track():
     track = hg.track("horizontal-line")
     isinstance(track, hg.EnumTrack)
     assert track.type == "horizontal-line"
+
+
+def test_viewport_projection():
+    v1 = hg.view(hg.track("heatmap"))
+    v2 = hg.view(hg.track("heatmap"))
+
+    v3 = v1.project(v2, on="top")
+    assert v3.tracks.top and v3.tracks.top[0].type == "viewport-projection-horizontal"
+
+    v4 = v2.project(v1, on="center", inplace=True)
+    assert v2 == v4
+    assert v2.tracks.center and v2.tracks.center[1].type == "viewport-projection-center"

0cf463b586b400f3fc5b3441d4d1452bded80147
Nezar Abdennur <nabdennur@gmail.com>
2023-02-26 22:20:42 -0500

    Add partition scale class

---
+++ src/higlass/_scale.py
@@ -0,0 +1,75 @@
+from __future__ import annotations
+from bisect import bisect_right
+from typing import Tuple
+import itertools
+
+
+GenomicPosition = Tuple[str, int]
+
+
+class Scale:
+    """
+    A bidirectional mapping between a composite genomic coordinate system and 
+    a partition of that coordinate system into a sequence of bins.
+
+    The partition is a 1D array of bins of a fixed size, with the exception of
+    the last bin in each chromosome, which may be smaller than the fixed size.
+    The scale provides a mapping from a genomic coordinate to the index of the
+    bin in which it falls, and a mapping from a bin index to the genomic 
+    coordinate of the bin's start.
+
+    Parameters
+    ----------
+    chromsizes : dict | list[tuple]
+        A dictionary of chromosome names and lengths or a list of tuples
+        of chromosome names and lengths.
+    binsize : int
+        The size of each bin in the partition in bp.
+
+    Notes
+    -----
+    The genomic coordinates are 0-based and the bins of the partition are 
+    half-open intervals, i.e. the start coordinate of a bin is included in the 
+    bin, but the end is not.
+    """
+    def __init__(
+        self, 
+        chromsizes: dict | list[tuple],
+        binsize: int = 1,
+    ):
+        chromsizes = dict(chromsizes)
+        names, lengths = list(chromsizes.keys()), list(chromsizes.values())
+        lengths_binned = [(length + binsize - 1) // binsize for length in lengths]
+        chrom_offsets = list(itertools.accumulate(lengths_binned, initial=0))
+        self._chrom_names = names
+        self._chrom_offsets = chrom_offsets
+        self._chrom_lengths_map = chromsizes
+        self._chrom_offsets_map = dict(zip(names, chrom_offsets[:-1]))
+        self.n_bins = chrom_offsets[-1]
+        self.binsize = binsize
+
+    def offset(self, gpos: GenomicPosition) -> int:
+        """
+        Returns the index of the bin in which the given genomic position falls.
+        """
+        chrom, pos = gpos
+        chrom_offset = self._chrom_offsets_map[chrom]
+        clen = self._chrom_lengths_map[chrom]
+        if pos < 0:
+            pos = 0
+        if pos >= clen:
+            pos = clen - 1
+        return chrom_offset + pos // self.binsize
+
+    def invert(self, offset: int) -> GenomicPosition:
+        """
+        Returns the genomic position of the start of the bin at the given index.
+        """
+        if offset < 0:
+            offset = 0
+        if offset >= self.n_bins:
+            offset = self.n_bins - 1
+        i = bisect_right(self._chrom_offsets, offset)
+        chrom = self._chrom_names[i - 1]
+        rel_offset = offset - self._chrom_offsets[i - 1]
+        return chrom, rel_offset * self.binsize
---
+++ test/test_scale.py
@@ -0,0 +1,55 @@
+import pytest
+from higlass._scale import Scale
+
+
+@pytest.fixture
+def scale():
+    chromsizes = [('chr1', 10000), ('chr2', 20000), ('chr3', 30000)]
+    binsize = 1000
+    return Scale(chromsizes, binsize)
+
+
+def get_data():
+    return [
+        (('chr1', 0), 0, ('chr1', 0)),
+        (('chr1', 900), 0, ('chr1', 0)),
+        (('chr1', 1000), 1, ('chr1', 1000)),
+        (('chr1', 9000), 9, ('chr1', 9000)),
+        (('chr1', 9999), 9, ('chr1', 9000)),
+        (('chr1', 10000), 9, ('chr1', 9000)),
+        (('chr1', 100000), 9, ('chr1', 9000)),
+        (('chr2', 0), 10, ('chr2', 0)),
+        (('chr2', 900), 10, ('chr2', 0)),
+        (('chr2', 1000), 11, ('chr2', 1000)),
+        (('chr2', 19000), 29, ('chr2', 19000)),
+        (('chr2', 19999), 29, ('chr2', 19000)),
+        (('chr2', 20000), 29, ('chr2', 19000)),
+        (('chr2', 100000), 29, ('chr2', 19000)),
+        (('chr3', 0), 30, ('chr3', 0)),
+        (('chr3', 900), 30, ('chr3', 0)),
+        (('chr3', 1000), 31, ('chr3', 1000)),
+        (('chr3', 29000), 59, ('chr3', 29000)),
+        (('chr3', 29999), 59, ('chr3', 29000)),
+        (('chr3', 30000), 59, ('chr3', 29000)),
+        (('chr3', 100000), 59, ('chr3', 29000)),
+    ]
+
+
+@pytest.mark.parametrize("gpos, offset, binstart", get_data())
+def test_offset(scale, gpos, offset, binstart):    
+    assert scale.offset(gpos) == offset
+
+
+@pytest.mark.parametrize("gpos, offset, binstart", get_data())
+def test_invert(scale, gpos, offset, binstart):
+    assert scale.invert(offset) == binstart
+
+
+def test_offset_out_of_bounds(scale):
+    assert scale.offset(('chr1', -1)) == 0
+    assert scale.offset(('chr3', 30000)) == 59
+
+
+def test_invert_out_of_bounds(scale):
+    assert scale.invert(-1) == ('chr1', 0)
+    assert scale.invert(60) == ('chr3', 29000)

2557f472b4fee39ec262d55c97ccea687c33672a
Trevor Manz <trevor.j.manz@gmail.com>
2025-02-11 15:08:01 -0500

    Make singleton `JupyterTilesetClient` with typed messages

--- src/higlass/_widget.py
+++ src/higlass/_widget.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 import concurrent.futures
+import functools
 import itertools
 import json
 import os
@@ -9,63 +10,88 @@ import typing
 
 import anywidget
 import ipywidgets
+import pydantic
 import traitlets as t
 
 from higlass._tileset_registry import TilesetRegistry
 
-if typing.TYPE_CHECKING:
-    from higlass.tilesets import LocalTileset
-
 __all__ = ["HiGlassWidget"]
 
-THREAD_EXECUTOR = concurrent.futures.ThreadPoolExecutor(max_workers=os.cpu_count())
+
+class TilesetInfo(pydantic.BaseModel):
+    """A tileset_info request payload."""
+
+    type: typing.Literal["tileset_info"]
+    tilesetUid: str
 
 
-def get_tileset(tileset_uid: str) -> LocalTileset:
-    tileset = TilesetRegistry.get(tileset_uid)
-    assert tileset, f"No tileset registered with id: {tileset_uid}"
-    return tileset
+class Tiles(pydantic.BaseModel):
+    """A tile request payload."""
+
+    type: typing.Literal["tiles"]
+    tileIds: list[str]
 
 
-def handle_custom_msg(widget, msg, buffers):
-    def respond_with(payload):
-        widget.send({"id": msg["id"], "payload": payload})
+class CustomMessage(pydantic.BaseModel):
+    """A custom message from the widget front end."""
 
-    def target():
-        match msg["payload"]:
-            case {"type": "tileset_info", "tilesetUid": tileset_uid}:
-                respond_with({tileset_uid: get_tileset(tileset_uid).info()})
-            case {"type": "tiles", "tileIds": tile_ids}:
+    id: str
+    payload: typing.Union[TilesetInfo, Tiles]  # noqa: UP007
+
+
+class JupyterTilesetClient(ipywidgets.Widget):
+    """A singleton client for handling tileset requests in a Jupyter environment.
+
+    Requests are handled asynchronously using a thread pool executor.
+    """
+
+    _executor = concurrent.futures.ThreadPoolExecutor(max_workers=os.cpu_count())
+
+    def __init__(self) -> None:
+        super().__init__()
+        self.on_msg(self._handle_custom_message)
+
+    @classmethod
+    @functools.lru_cache(maxsize=1)
+    def get_instance(cls):
+        """Return a singleton client."""
+        return cls()
+
+    def _handle_custom_message(self, widget, msg, buffers):
+        message = CustomMessage(**msg)
+
+        def respond_with(payload: object):
+            self.send({"id": message.id, "payload": payload})
+
+        def process_message():
+            if isinstance(message.payload, TilesetInfo):
+                tileset_uid = message.payload.tilesetUid
+                respond_with({tileset_uid: TilesetRegistry.get(tileset_uid).info()})
+
+            elif isinstance(message.payload, Tiles):
+                tile_ids = message.payload.tileIds
                 tiles = []
-                for tileset_uid, group_tile_ids in itertools.groupby(
+                for tileset_uid, group in itertools.groupby(
                     iterable=sorted(tile_ids), key=lambda tile_id: tile_id.split(".")[0]
                 ):
-                    tiles.extend(get_tileset(tileset_uid).tiles(list(group_tile_ids)))
+                    tiles.extend(TilesetRegistry.get(tileset_uid).tiles(list(group)))
                 respond_with({tile_id: tile for tile_id, tile in tiles})
-            case _:
-                message = f"Invalid request, got: {msg.get('type', 'unknown')}"
-                raise ValueError(message)
 
-    THREAD_EXECUTOR.submit(target)
+            else:
+                raise RuntimeError("Unexpected execution path")
 
-
-class TilesetClient(ipywidgets.DOMWidget):
-    def __init__(self):
-        super().__init__()
-        self.on_msg(handle_custom_msg)
-
-
-# a singleton for all the tilesets
-_TILESET_CLIENT = TilesetClient()
+        self._executor.submit(process_message)
 
 
 class HiGlassWidget(anywidget.AnyWidget):
+    """An interactive anywidget for HiGlass."""
+
     _esm = pathlib.Path(__file__).parent / "widget.js"
 
     _viewconf = t.Dict(allow_none=False).tag(sync=True)
     _options = t.Dict().tag(sync=True)
-    _tileset_client = t.Any().tag(sync=True, **ipywidgets.widget_serialization)
     _plugin_urls = t.List().tag(sync=True)
+    _tileset_client = t.Any().tag(sync=True, **ipywidgets.widget_serialization)
 
     # readonly properties
     location = t.List(t.Union([t.Float(), t.Tuple()]), read_only=True).tag(sync=True)
@@ -73,9 +99,9 @@ class HiGlassWidget(anywidget.AnyWidget):
     def __init__(self, viewconf: dict, plugin_urls: list[str] | None, **viewer_options):
         super().__init__(
             _viewconf=viewconf,
-            _tileset_client=_TILESET_CLIENT,
             _plugin_urls=plugin_urls,
             _options=viewer_options,
+            _tileset_client=JupyterTilesetClient.get_instance(),
         )
 
     def reload(self, *items):

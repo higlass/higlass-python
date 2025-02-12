0324bc43db73ea166dd38ea901400086689effb6
Trevor Manz <trevor.j.manz@gmail.com>
2025-02-12 16:19:53 -0500

    feat: Add stub `higlass.server` with deprecation warnings

--- src/higlass/__init__.py
+++ src/higlass/__init__.py
@@ -12,4 +12,8 @@ from higlass_schema import *
 
 import higlass.tilesets as tilesets
 from higlass.api import *
+from higlass.server import HiGlassServer
 from higlass.tilesets import bed2ddb, beddb, bigwig, cooler, hitile, multivec, remote
+
+# a stub server with some helpful warnings
+server = HiGlassServer()
